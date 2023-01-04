cc2add048ce31ba6a856f998e717d454847d4136
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-04 16:04:52 +0100

    Change Experiment 03 to use top-20 workload instead of UES native
    This change aligns the pipeline with the data presented in the paper. It
    used the top-20 workload to demonstrate the broad range of different
    settings that can be explored with PostBOUND.

--- btw-scripts/btw-exp03.sh
+++ btw-scripts/btw-exp03.sh
@@ -10,15 +10,34 @@ cd $ROOT/postgres
 echo "... Setting up Postgres environment"
 . ./postgres-start.sh
 
+cd $ROOT/ues
+echo "... Generating UES workload with subqueries"
+./ues-generator.py --pattern "*.sql" --generate-labels --out-col query --join-paths --timing \
+    --table-estimation precise \
+    --join-estimation topk-approx --topk-length 20 \
+    --subqueries smart \
+    --out-col query --out workloads/topk-setups/job-ues-workload-topk-20-approx-smart.csv \
+    $ROOT/workloads/JOB-Queries/implicit
+
 echo "... Generating linear UES workload"
 ./ues-generator.py --pattern "*.sql" --generate-labels --out-col query --join-paths --timing \
     --table-estimation precise \
+    --join-estimation topk-approx --topk-length 20 \
     --subqueries disabled \
-    --out-col query --out workloads/job-ues-workload-base-linear.csv \
+    --out-col query --out workloads/topk-setups/job-ues-workload-topk-20-approx-linear.csv \
     $ROOT/workloads/JOB-Queries/implicit
 
+echo "... Running UES workload with subqueries"
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --experiment-mode ues \
+    --out workloads/topk-setups/job-ues-results-topk-20-approx-smart.csv \
+    workloads/topk-setups/job-ues-workload-topk-20-approx-smart.csv
+
 echo "... Running linear UES workload"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --experiment-mode ues --out workloads/job-ues-results-base-linear.csv workloads/job-ues-workload-base-linear.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --experiment-mode ues \
+    --out workloads/topk-setups/job-ues-results-topk-20-approx-linear.csv \
+    workloads/topk-setups/job-ues-workload-topk-20-approx-linear.csv
 
 echo "... Gathering results"
 # TODO: how/what should we do here exactly?
