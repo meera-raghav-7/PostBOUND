ba3a5e8137f7b188a3a7570d0b87dfc7d908240e
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-05 16:24:36 +0100

    Docker baseline for BTW repro

---
+++ btw-scripts/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+mkdir -p ../results
+docker build -t btw-postbound .
+docker run -dt \
+    --name btw-postbound \
+    --mount type=bind,source=$(pwd)/../results,target=/postbound-btw23/BTW23-PostBOUND/results \
+    btw-postbound
+docker exec -t btw-postbound /postbound-btw23/btw-start.sh
