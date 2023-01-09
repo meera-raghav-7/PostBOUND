d50723c57faa07401f7bef5a9abfd5412d183704
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-09 18:30:55 +0100

    Handle unset $USER, re-use imdb_data if possible

--- btw-scripts/run.sh
+++ btw-scripts/run.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+DOCKER_ROOT=/postbound-btw23
+USER=$(whoami)
+
 mkdir -p results
 docker build -t btw-postbound --build-arg user=$USER . | tee docker-setup.log
 docker run -dt \
     --name btw-postbound \
-    --mount type=bind,source=$(pwd)/results,target=/postbound-btw23/BTW23-PostBOUND/results \
+    --mount type=bind,source=$(pwd)/results,target=$DOCKER_ROOT/BTW23-PostBOUND/results \
+    --user $USER \
     btw-postbound
-docker exec -t btw-postbound /postbound-btw23/btw-start.sh | tee experiments.log
+
+if [ -d "../imdb_data" ] ; then
+    docker container cp ../imdb_data btw-postbound:$DOCKER_ROOT
+fi
+
+docker exec -t --user $USER btw-postbound $DOCKER_ROOT/btw-start.sh | tee experiments.log
