d233b3307dcdca2ff671c51fdb606d81b473e9cb
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-09 18:26:47 +0100

    Run native PG experiments without NLJ
    This aligns the experiment setup with the settings described in the
    actual paper.

--- btw-scripts/btw-exp02.sh
+++ btw-scripts/btw-exp02.sh
@@ -23,16 +23,27 @@ cd $ROOT/ues
 ./set-workload.sh job
 
 echo "... Generating native query workload for JOB"
-$ROOT/util/generate-workload.py --pattern "*.sql" --generate-labels --out workloads/job-workload-implicit.csv $ROOT/workloads/JOB-Queries/implicit
+$ROOT/util/generate-workload.py --pattern "*.sql" --generate-labels \
+    --out workloads/job-workload-implicit.csv \
+    $ROOT/workloads/JOB-Queries/implicit
 
 echo "... Running native workload for JOB, PG v14"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --out workloads/job-results-implicit.csv workloads/job-workload-implicit.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --pg-param "SET enable_nestloop = 'off';" \
+    --out workloads/job-results-implicit.csv \
+    workloads/job-workload-implicit.csv
 
 echo "... Generating UES queries for JOB"
-./ues-generator.py --pattern "*.sql" --generate-labels --out-col query --join-paths --timing --out workloads/job-ues-workload-base.csv $ROOT/workloads/JOB-Queries/implicit
+./ues-generator.py --pattern "*.sql" --generate-labels \
+    --join-paths --timing \
+    --out-col query --out workloads/job-ues-workload-base.csv \
+    $ROOT/workloads/JOB-Queries/implicit
 
 echo "... Running UES workload for JOB, PG v14"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --experiment-mode ues --out workloads/job-ues-results-base.csv workloads/job-ues-workload-base.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --experiment-mode ues \
+    --out workloads/job-ues-results-base.csv \
+    workloads/job-ues-workload-base.csv
 
 cd $ROOT/postgres
 echo "... Cleaning up Postgres v14 environment"
@@ -49,10 +60,16 @@ util/setup-job.sh
 
 cd $ROOT/ues
 echo "... Running native workload for JOB, PG v12"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --out workloads/job-results-implicit-pg12_4.csv workloads/job-workload-implicit.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --pg-param "SET enable_nestloop = 'off';" \
+    --out workloads/job-results-implicit-pg12_4.csv \
+    workloads/job-workload-implicit.csv
 
 echo "... Running UES workload for JOB, PG v14"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --experiment-mode ues --out workloads/job-ues-results-base-pg12_4.csv workloads/job-ues-workload-base.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --experiment-mode ues \
+    --out workloads/job-ues-results-base-pg12_4.csv \
+    workloads/job-ues-workload-base.csv
 
 cd $ROOT/postgres_12_4
 echo "... Cleaning up Postgres v12 environment"
@@ -77,16 +94,27 @@ cd $ROOT/ues
 ./set-workload.sh ssb
 
 echo "... Generating native query workload for SSB"
-$ROOT/util/generate-workload.py --pattern "*.sql" --generate-labels --out workloads/ssb-workload-implicit.csv $ROOT/workloads/SSB-Queries
+$ROOT/util/generate-workload.py --pattern "*.sql" --generate-labels \
+    --out workloads/ssb-workload-implicit.csv \
+    $ROOT/workloads/SSB-Queries
 
 echo "... Running native workload for SSB, PG v14"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --out workloads/ssb-results-implicit.csv workloads/ssb-workload-implicit.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --pg-param "SET enable_nestloop = 'off';" \
+    --out workloads/ssb-results-implicit.csv \
+    workloads/ssb-workload-implicit.csv
 
 echo "... Generating UES queries for SSB"
-./ues-generator.py --pattern "*.sql" --generate-labels --out-col query --join-paths --timing --out workloads/ssb-ues-workload-base.csv $ROOT/workloads/SSB-Queries
+./ues-generator.py --pattern "*.sql" --generate-labels \
+    --join-paths --timing \
+    --out-col query --out workloads/ssb-ues-workload-base.csv \
+    $ROOT/workloads/SSB-Queries
 
 echo "... Running UES workload for SSB, PG v14"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --experiment-mode ues --out workloads/ssb-ues-results-base.csv workloads/ssb-ues-workload-base.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --experiment-mode ues \
+    --out workloads/ssb-ues-results-base.csv \
+    workloads/ssb-ues-workload-base.csv
 
 cd $ROOT/postgres
 echo "... Cleaning up Postgres v14 environment"
@@ -103,10 +131,16 @@ util/setup-ssb.sh
 
 cd $ROOT/ues
 echo "... Running native workload for SSB, PG v12"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --out workloads/ssb-results-implicit-pg12_4.csv workloads/ssb-workload-implicit.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --pg-param "SET enable_nestloop = 'off';" \
+    --out workloads/ssb-results-implicit-pg12_4.csv \
+    workloads/ssb-workload-implicit.csv
 
 echo "... Running UES workload for JOB, PG v14"
-./experiment-runner.py --csv --per-query-repetitions 3 --query-mod analyze --experiment-mode ues --out workloads/ssb-ues-results-base-pg12_4.csv workloads/ssb-ues-workload-base.csv
+./experiment-runner.py --csv --per-query-repetitions 3 \
+    --query-mod analyze --experiment-mode ues \
+    --out workloads/ssb-ues-results-base-pg12_4.csv \
+    workloads/ssb-ues-workload-base.csv
 
 cd $ROOT/postgres_12_4
 echo "... Cleaning up Postgres v12 environment"
