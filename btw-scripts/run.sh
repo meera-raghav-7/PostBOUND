9ae1a962e64f62a9c915e2badfe05dc8f3313cc4
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-10 09:02:28 +0100

    Increase Docker container shared mem to 2G
    The default of 64 MB is too small to execute larger postgres workloads
    reliably.

--- btw-scripts/run.sh
+++ btw-scripts/run.sh
@@ -8,11 +8,9 @@ docker build -t btw-postbound --build-arg user=$USER . | tee docker-setup.log
 docker run -dt \
     --name btw-postbound \
     --mount type=bind,source=$(pwd)/results,target=$DOCKER_ROOT/BTW23-PostBOUND/results \
+    --shm-size 2G \
     --user $USER \
     btw-postbound
 
-if [ -d "../imdb_data" ] ; then
-    docker container cp ../imdb_data btw-postbound:$DOCKER_ROOT
-fi
 
 docker exec -t --user $USER btw-postbound $DOCKER_ROOT/btw-start.sh | tee experiments.log
