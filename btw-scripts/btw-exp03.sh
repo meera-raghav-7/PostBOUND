910c6777c7df5b0cde8d594d24d99c76681c6c0a
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-12 11:08:07 +0100

    Add script to prepare evaluation files for topk-setups

--- btw-scripts/btw-exp03.sh
+++ btw-scripts/btw-exp03.sh
@@ -46,6 +46,10 @@ echo "... Running linear UES workload"
     --out workloads/topk-setups/job-ues-results-topk-20-approx-linear.csv \
     workloads/topk-setups/job-ues-workload-topk-20-approx-linear.csv
 
+cd $ROOT/ues
+echo "... Generating result files"
+python3 evaluation/aggregate-topk-results.py
+
 cd $ROOT/postgres
 echo "... Cleaning up"
 . ./postgres-stop.sh
