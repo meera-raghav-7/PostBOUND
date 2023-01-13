971ff186e56c24a6ca6c0538ec8a21685876ec2d
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-13 18:42:08 +0100

    Add much more time logging to main experiments script

--- btw-scripts/btw-start.sh
+++ btw-scripts/btw-start.sh
@@ -14,7 +14,7 @@ if [ -d $TARGET_DIR ] ; then
     cd $TARGET_DIR
     git pull --recurse-submodules=yes origin btw23-reproducibility
 else
-    echo ".. Setting up environment"
+    echo ".. Setting up environment on $(date)"
     echo ".. Target directory is $TARGET_DIR"
     git clone --recurse-submodules --branch btw23-reproducibility --depth 1 https://github.com/rbergm/PostBOUND.git temp
     mv temp/* $TARGET_DIR
@@ -22,24 +22,24 @@ else
     rm -r temp
 fi
 
-echo ".. Preparing directory structure"
+echo ".. Preparing directory structure on $(date)"
 ROOT=$ORIG_ROOT/$TARGET_DIR
 cd $ROOT
 cp $ORIG_ROOT/btw-exp*.sh .
 cp $ORIG_ROOT/btw-setup.R .
 cp $ORIG_ROOT/btw-tex.sh .
 
-echo ".. Setting up Python venv"
+echo ".. Setting up Python venv on $(date)"
 python3 -m venv postbound-venv
 . postbound-venv/bin/activate
 pip install --progress-bar off wheel
 pip install --progress-bar off -r requirements.txt
 
-echo ".. Setting up R environment"
+echo ".. Setting up R environment on $(date)"
 Rscript --vanilla btw-setup.R
 
 cd $ROOT/postgres
-echo ".. Setting up Postgres v14"
+echo ".. Setting up Postgres v14 on $(date)"
 ./postgres-setup.sh --stop
 ./postgres-psycopg-setup.sh job imdb
 ./postgres-psycopg-setup.sh stack stack_dummy
@@ -47,36 +47,36 @@ echo ".. Setting up Postgres v14"
 mv .psycopg_connection_* $ROOT/ues
 
 cd $ROOT/postgres_12_4
-echo ".. Setting up Postgres v12"
+echo ".. Setting up Postgres v12 on $(date)"
 ./postgres-setup.sh --stop
 
 cd $ROOT
-echo ".. Starting Experiment 01 :: Figure 01 - UES overestimation"
+echo ".. Starting Experiment 01 on $(date) :: Figure 01 - UES overestimation"
 ./btw-exp01.sh
 
 cd $ROOT
-echo ".. Starting Experiment 02 :: Table 01 - Benchmark runtimes"
+echo ".. Starting Experiment 02 on $(date) :: Table 01 - Benchmark runtimes"
 ./btw-exp02.sh
 
 cd $ROOT
-echo ".. Starting Experiment 03 :: Figure 07 - Subquery speedup"
+echo ".. Starting Experiment 03 on $(date) :: Figure 07 - Subquery speedup"
 ./btw-exp03.sh
 
 cd $ROOT
-echo ".. Starting Experiment 04 :: Figure 08 - top-k influence"
+echo ".. Starting Experiment 04 on $(date) :: Figure 08 - top-k influence"
 ./btw-exp04.sh
 
 cd $ROOT
-echo ".. Starting Experiment 05 :: Figure 09 - IdxNLJ operators"
+echo ".. Starting Experiment 05 on $(date) :: Figure 09 - IdxNLJ operators"
 ./btw-exp05.sh
 
 cd $ROOT/ues
-echo ".. Generating result figures"
+echo ".. Generating result figures on $(date)"
 python3 postbound-eval.py
 Rscript --vanilla evaluation/plots-presentation.R
 
 cd $ROOT
-echo ".. Creating final paper"
+echo ".. Creating final paper on $(date)"
 ./btw-tex.sh
 
 cd $ORIG_ROOT
