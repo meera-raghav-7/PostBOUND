5e656819442fc1fd0dbc2656d103dd5da4598884
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-13 18:36:41 +0100

    Try different approach for exporting results
    Bind mounts don't seem to work well for users other than root

--- btw-scripts/run.sh
+++ btw-scripts/run.sh
@@ -3,14 +3,13 @@
 DOCKER_ROOT=/postbound-btw23
 USER=$(whoami)
 
-mkdir -p results/raw
 docker build -t btw-postbound --build-arg user=$USER . | tee docker-setup.log
 docker run -dt \
     --name btw-postbound \
-    --mount type=bind,source=$(pwd)/results,target=$DOCKER_ROOT/BTW23-PostBOUND/results \
     --shm-size 2G \
     --user $USER \
     btw-postbound
 
 
 docker exec -t --user $USER btw-postbound $DOCKER_ROOT/btw-start.sh | tee experiments.log
+docker container cp btw-postbound btw-postbound:$DOCKER_ROOT/BTW23-PostBOUND/results .
