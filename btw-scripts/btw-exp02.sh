6e601ddf62b7bc65aaf6bc0b04cce2c20192132f
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2023-01-13 15:46:31 +0100

    Another multiline escape fix...

--- btw-scripts/btw-exp02.sh
+++ btw-scripts/btw-exp02.sh
@@ -38,7 +38,7 @@ echo "... Generating UES queries for JOB"
 ./ues-generator.py --pattern "*.sql" --generate-labels \
     --join-paths --timing \
     --table-estimation precise \
-    --subqueries smart
+    --subqueries smart \
     --out-col query --out workloads/job-ues-workload-base.csv \
     $ROOT/workloads/JOB-Queries/implicit
 
